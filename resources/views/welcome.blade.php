9b9938894cf384d6b42de4fc37ce687a3c8f15f4
Gioele Coviello <gioelecov@gmail.com>
2023-04-17 15:23:38 +0200

    inizio progetto: aggiunta navbar, immagine di sfondo e titolo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<x-layout>
+
+
+
+  <!-- Full Page Image Header with Vertically Centered Content -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-12 text-center">
+          <h1 class="fw-light fw-bold text-white">Videogiuoco Project</h1>
+          <p class="lead fw-bold text-white">Benvenuti nella nostra pagina di videogiochi</p>
+        </div>
+      </div>
+    </div>
+  </header>
+  
+ 
+
+
+
+
+
+
+</x-layout>
